1bd52ece4504481e4a658c4524262c8c632451de
Nasan Soonthondachar <63160070@go.buu.ac.th>
2022-05-01 22:28:28 +0700

    set provider and listeners

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../screens/product_detail_screen.dart';
 import './screens/products_overview_screen.dart';
+import './providers/products_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-          // primarySwatch: Colors.purple,
-          colorScheme: ColorScheme.fromSwatch()
-              .copyWith(secondary: Colors.deepOrange, primary: Colors.purple),
-          fontFamily: 'Lato'),
-      home: ProductsOverviewScreen(),
+    return ChangeNotifierProvider(
+      create: (context) => ProductsProvider(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+            // primarySwatch: Colors.purple,
+            colorScheme: ColorScheme.fromSwatch()
+                .copyWith(secondary: Colors.deepOrange, primary: Colors.purple),
+            fontFamily: 'Lato'),
+        home: ProductsOverviewScreen(),
+        routes: {ProductDetailScreen.routeName: (ctx) => ProductDetailScreen()},
+      ),
     );
   }
 }
