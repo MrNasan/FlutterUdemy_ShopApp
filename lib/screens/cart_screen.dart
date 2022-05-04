747647628c885aad0dd333e474d0e254baa3cec9
Nasan Soonthondachar <63160070@go.buu.ac.th>
2022-05-04 14:43:32 +0700

    Display list of cart in cart screen

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_app/providers/cart.dart';
+import 'package:shop_app/providers/cart.dart' show Cart;
+import '../widgets/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({Key? key}) : super(key: key);
@@ -49,7 +50,20 @@ class CartScreen extends StatelessWidget {
                 ],
               ),
             ),
-          )
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Expanded(
+              child: ListView.builder(
+            itemBuilder: ((context, index) => CartItem(
+                  cart.items.values.toList()[index].id,
+                  cart.items.values.toList()[index].title,
+                  cart.items.values.toList()[index].price,
+                  cart.items.values.toList()[index].quantity,
+                )),
+            itemCount: cart.items.length,
+          ))
         ],
       ),
     );
